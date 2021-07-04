010c35c1d5e8e1c94b3e0384a09e1b8acfc9a971
Sönke Ludwig <sludwig@outerproduct.org>
2021-07-04 20:40:45 +0200

    Bump version to 1.18.1.

--- source/vibe/core/core.d
+++ source/vibe/core/core.d
@@ -1458,7 +1458,7 @@ void setTaskCreationCallback(TaskCreationCallback func)
 /**
 	A version string representing the current vibe.d core version
 */
-enum vibeVersionString = "1.17.1";
+enum vibeVersionString = "1.18.1";
 
 
 /**
