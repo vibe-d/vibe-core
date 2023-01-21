d62b58e906e9221e04b6993fdf5de65036927713
Sönke Ludwig <sludwig@outerproduct.org>
2023-01-21 13:14:38 +0100

    Make args test work on Windows.

--- tests/args.sh
+++ tests/args.sh
@@ -3,8 +3,8 @@
 set -e
 die() { echo "$@" 1>&2 ; exit 1; }
 
-( dub --single args.d | grep -q '^argtest=$' ) || die "Fail (no argument): '`dub --single args.d`'"
-( dub --single args.d -- --argtest=aoeu | grep -q '^argtest=aoeu$' ) || die "Fail (with argument): '`dub --single args.d -- --argtest=aoeu`'"
-( ( ! dub --single args.d -- --inexisting 2>&1 ) | grep -qF 'Unrecognized command line option' ) || die "Fail (unknown argument): '`dub --single args.d -- --inexisting 2>&1`'"
+( dub --single args.d | sed 's/\x0D$//' | grep -q '^argtest=$' ) || die "Fail (no argument): '`dub --single args.d`'"
+( dub --single args.d -- --argtest=aoeu | sed 's/\x0D$//' | grep -q '^argtest=aoeu$' ) || die "Fail (with argument): '`dub --single args.d -- --argtest=aoeu`'"
+( ( ! dub --single args.d -- --inexisting 2>&1 ) | sed 's/\x0D$//' | grep -qF 'Unrecognized command line option' ) || die "Fail (unknown argument): '`dub --single args.d -- --inexisting 2>&1`'"
 
 echo 'OK'
