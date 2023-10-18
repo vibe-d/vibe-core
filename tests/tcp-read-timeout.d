833d071fe4d16b922c7d42725620e42e3d616a3e
Sönke Ludwig <sludwig@outerproduct.org>
2023-10-18 14:31:34 +0200

    Fix the test case and skip running it on Windows due to cancelRead() bug.

--- tests/tcp-read-timeout.d
+++ tests/tcp-read-timeout.d
@@ -1,90 +1,72 @@
+/+ dub.sdl:
+	dependency "vibe-core" path=".."
++/
 module tests;
 
-import vibe.core.core ;
+import vibe.core.core;
 import vibe.core.log;
 import vibe.core.net;
-import vibe.core.stream : pipe;
 
-
-import std.exception;
-import std.datetime;
-import std.stdio;
-import std.datetime.stopwatch;
-import  std.algorithm.comparison;
 import eventcore.driver : IOMode;
+import std.algorithm.comparison;
+import std.exception;
+import std.datetime.stopwatch;
+import std.stdio;
 
+void handleTcp(ref TCPConnection stream)
+@trusted nothrow {
+	assumeWontThrow(stream.readTimeout = dur!"msecs"(500));
 
+	ubyte[] buffer = new ubyte[2048];
+	bool got_timeout = false;
 
-scope const(ubyte[]) readTcpOnce(ref TCPConnection stream,scope ubyte[] dst){
-    ubyte[] ret = null;
-    auto sw = StopWatch(AutoStart.yes);
-    auto e = collectException!ReadTimeoutException({
-        auto len = stream.read(dst, IOMode.once);
-        if(len == 0)
-            return null;
-        else
-            return dst[0..len];
-    }(),ret);
-    sw.stop();
-    if(e) {
-        long msecs = sw.peek.total!"msecs";
-        collectException({logInfo("%d : %s ",msecs,e.msg);}());
-        if(msecs < 500){
-             logError("Test failed:read time out should >= timeout time : %d", msecs);
-        }
-    }
+	while (true) {
+		ubyte[] tmp;
+		auto sw = StopWatch(AutoStart.yes);
+		try {
+			tmp = buffer[0 .. stream.read(buffer, IOMode.once)];
+		} catch (ReadTimeoutException e) {
+			assert (sw.peek >= 500.msecs, "Timeout occurred too early");
+			got_timeout = true;
+		} catch (Exception e) {
+			assert(false, "Unexpected exception on server side: " ~ e.msg);
+		}
 
-    return ret;
+		if (tmp == "end") break;
+		else stream.write(tmp).assumeWontThrow;
+	}
+
+	assert(got_timeout, "Expected timeout did not occur");
+	stream.close();
 }
 
-void handleTcp(ref TCPConnection stream) @trusted nothrow {
-    auto e = collectException({
-        stream.readTimeout = dur!"msecs"(500);
-        ubyte[] buffer = new ubyte[2048];
-        while(stream.connected){
-            auto tmp = stream.readTcpOnce(buffer);
-            if(tmp !is null){
-                if(cmp(tmp,"end") == 0){
-                    stream.close();
-                    break;
-                }
-                stream.write(tmp);
-            }
-        }
-    }());
-    if(e){
-        logError("Test failed:read not should has other exception : %s", e.toString());
-    }
-}
 void main()
 {
-    auto listeners = listenTCP(17000,  (conn) @safe nothrow {handleTcp(conn);});
+	version (Windows) logWarn("SKIPPING TEST DUE TO EVENTCORE BUG #225");
+	else {
+		auto tm = setTimer(10.seconds, { assert(false, "Test timed out"); });
 
-    // closes the listening sockets
-    scope (exit)
-        foreach (l; listeners)
-            l.stopListening();
+		auto listeners = listenTCP(17000, (conn) @safe nothrow {handleTcp(conn);});
 
-    runTask(() @trusted
-    {
-        collectExceptionMsg({
-        // sleep one seconds let server start
-            sleep(dur!"seconds"(1));
-            ubyte[] buffer = new ubyte[512];
-            auto client = connectTCP("127.0.0.1",17000);
-            assert(client.connected,"connect server error!");
-            auto send = "hello word";
-            client.write(send);
-            auto readed = client.read(buffer,IOMode.once);
-            auto tmp = buffer[0..readed];
-            if(cmp(tmp,send) != 0){
-                logError("Test failed: client read echo error!");
-            }
-            sleep(dur!"seconds"(5));
-            client.write("end");
-            sleep(dur!"msecs"(800));
-        }());
-        exitEventLoop();
-    });
-    runEventLoop();
+		// closes the listening sockets
+		scope (exit)
+			foreach (l; listeners)
+				l.stopListening();
+
+		// sleep one seconds let server start
+		sleep(dur!"seconds"(1));
+		ubyte[] buffer = new ubyte[512];
+		auto client = connectTCP("127.0.0.1", 17000);
+		client.readTimeout = 500.msecs;
+		assert(client.connected, "connect server error!");
+		auto send = "hello word";
+		client.write(send);
+		auto readed = client.read(buffer,IOMode.once);
+		auto tmp = buffer[0 .. readed];
+		assert(tmp == send, "Client received unexpected echo data");
+		sleep(700.msecs);
+		client.write("end");
+		assert(client.empty);
+		client.close();
+	}
 }
