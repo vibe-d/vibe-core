d9d82ce5bd32c80932c9911d174315e77b26659e
Sönke Ludwig <sludwig@outerproduct.org>
2023-11-24 17:25:20 +0100

    Bump version to 2.5.1.

--- source/vibe/core/core.d
+++ source/vibe/core/core.d
@@ -1355,7 +1355,7 @@ void setTaskCreationCallback(TaskCreationCallback func)
 /**
 	A version string representing the current vibe.d core version
 */
-enum vibeVersionString = "2.5.0";
+enum vibeVersionString = "2.5.1";
 
 
 /**
