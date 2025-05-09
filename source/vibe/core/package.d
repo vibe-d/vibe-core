1dce395fe15f536f58f7c37b47e4285ce0926073
Sönke Ludwig <sludwig@outerproduct.org>
2025-05-09 10:55:52 +0200

    Add `vibe.core` convenience module for importing the whole API at once.

---
+++ source/vibe/core/package.d
@@ -0,0 +1,24 @@
+/**
+	Makes the complete vibe-core API available with a single import
+
+	Copyright: © 2025 Sönke Ludwig
+	License: Subject to the terms of the MIT license, as written in the included LICENSE.txt file.
+	Authors: Sönke Ludwig
+*/
+module vibe.core;
+
+public import vibe.core.args;
+public import vibe.core.channel;
+public import vibe.core.concurrency;
+public import vibe.core.connectionpool;
+public import vibe.core.core;
+public import vibe.core.file;
+public import vibe.core.log;
+public import vibe.core.net;
+public import vibe.core.parallelism;
+public import vibe.core.path;
+public import vibe.core.process;
+public import vibe.core.stream;
+public import vibe.core.sync;
+public import vibe.core.task;
+public import vibe.core.taskpool;
